4da6598e832e419b86130e7a3784705f11760cff
“sileyouhe” <sileyouhe@gmail.com>
2023-05-06 21:30:40 -0400

    Add Generic Exception Handlers

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, session
+from werkzeug.exceptions import HTTPException
+import logging as logger
 from user import user_bp
 from song import song_bp
 from artist import artist_bp
@@ -18,6 +20,8 @@ def hello():
     print("user = ", user)
     return render_template('index.html', genres=genres, user=user)
 
+
+
 def get_genre():
     cursor = conn.cursor()
     query = 'SELECT distinct genre FROM songgenre'
@@ -26,6 +30,14 @@ def get_genre():
     cursor.close()
     return genres
 
+@app.errorhandler(Exception)
+def handle_exception(e):
+    # pass through HTTP errors
+    if isinstance(e, HTTPException):
+        return e
+
+    # now you're handling non-HTTP exceptions only
+    return render_template("500_generic.html", e=e), 500
 
 # if __name__ == '__main__':
 #     app.debug = True
